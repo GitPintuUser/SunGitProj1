89b7a363cb0f5ecfd93bb8d97ede5e4d5f3474ba
owner <dev2@gmail.com>
2018-06-10 13:13:08 +0530

    Devloper1--->addition

--- Test/src/com/nt/test/Client.java
+++ Test/src/com/nt/test/Client.java
@@ -4,7 +4,11 @@ public class Client {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Welcome to Git");
+		
+		System.out.println("Addition of two number :"+MathOperation.add(10,20));
 
 	}
+	
+	
 
 }
---
+++ Test/src/com/nt/test/MathOperation.java
@@ -0,0 +1,8 @@
+package com.nt.test;
+
+public class MathOperation {
+	public static int add(int fno,int sno){
+		return fno+sno;
+	}
+
+}
