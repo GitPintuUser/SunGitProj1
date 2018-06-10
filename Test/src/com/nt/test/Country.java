dc36082d8a5b02d96cb016e1f549732f5151d2b3
owner <dev2@gmail.com>
2018-06-10 15:12:33 +0530

    Adding getCountry method to Country class

--- Test/src/com/nt/test/Country.java
+++ Test/src/com/nt/test/Country.java
@@ -1,5 +1,14 @@
 package com.nt.test;
 
+import java.util.Locale;
+
 public class Country {
+	public void getCountry(){
+		Locale locales[]=Locale.getAvailableLocales();
+		
+		for(Locale locale:locales){
+			System.out.println("Country :"+locale.getCountry());
+		}
+	}
 
 }
