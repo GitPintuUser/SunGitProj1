0759aa5cb6bae6c79ee4135a21e692378cb83ad7
owner <dev2@gmail.com>
2018-06-10 15:25:44 +0530

    Modification done by developer2

--- Test/src/com/nt/test/Country.java
+++ Test/src/com/nt/test/Country.java
@@ -5,9 +5,10 @@ import java.util.Locale;
 public class Country {
 	public static void getCountry(){
 		Locale locales[]=Locale.getAvailableLocales();
-		
+		System.out.println("Country Name \u0000 Language");
+		System.out.println("---------- \t ----------");
 		for(Locale locale:locales){
-			System.out.println("Country :"+locale.getCountry());
+			System.out.println(locale.getDisplayCountry()+"\t\t"+locale.getDisplayLanguage());
 		}
 	}
 
